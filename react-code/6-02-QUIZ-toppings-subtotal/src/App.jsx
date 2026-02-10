6dfa96290648963d4d09873d3951030420c01266
ahmed-abohmaid <ahmedmedo.am121212@gmail.com>
2026-02-10 15:23:33 +0200

    chore: push starter code of the course

---
+++ react-code/6-02-QUIZ-toppings-subtotal/src/App.jsx
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+
+import OrderEntry from "./pages/entry/OrderEntry";
+
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+
+function App() {
+  return (
+    <OrderDetailsProvider>
+      <Container>
+        <OrderEntry />
+      </Container>
+    </OrderDetailsProvider>
+  );
+}
+
+export default App;
