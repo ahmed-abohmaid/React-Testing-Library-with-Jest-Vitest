c7d8fca5ca33e9513bae9b5677684b2741cbd3cc
ahmed-abohmaid <ahmedmedo.am121212@gmail.com>
2026-02-11 20:27:14 +0200

    chore: update ESLint configuration and dependencies for improved testing support

--- sundae-starter/.eslintrc.cjs
+++ sundae-starter/.eslintrc.cjs
@@ -1,4 +1,6 @@
-const vitest = require("eslint-plugin-vitest");
+import jestDom from "eslint-plugin-jest-dom";
+import testingLibrary from "eslint-plugin-testing-library";
+import vitest from "eslint-plugin-vitest";
 
 module.exports = {
   root: true,
@@ -11,11 +13,17 @@ module.exports = {
     "plugin:testing-library/react",
     "plugin:vitest/recommended",
     "plugin:jest-dom/recommended",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: {
+    "react-refresh": "react-refresh",
+    "jest-dom": jestDom,
+    "testing-library": testingLibrary,
+    vitest,
+  },
   rules: {
     "react-refresh/only-export-components": [
       "warn",
@@ -24,6 +32,9 @@ module.exports = {
     "no-unused-vars": "warn", // warning, not error
     "vitest/expect-expect": "off", // distracting red squiggles while writing tests
     "react/prop-types": "off", // turn off props validation
+    ...jestDom.configs.recommended.rules,
+    ...testingLibrary.configs.react.rules,
+    ...vitest.configs.recommended.rules,
   },
   globals: {
     ...vitest.environments.env.globals,
