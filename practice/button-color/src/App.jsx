230682fae715c09cf7cc2a5cf5f43272b4c75ac7
ahmed-abohmaid <ahmedmedo.am121212@gmail.com>
2026-02-11 10:20:57 +0200

    feat: add button disable functionality and corresponding tests

--- practice/button-color/src/App.jsx
+++ practice/button-color/src/App.jsx
@@ -3,15 +3,29 @@ import { useState } from "react";
 
 function App() {
   const [isRed, setIsRed] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   return (
     <div>
       <button
         className={isRed ? "red" : "blue"}
         onClick={() => setIsRed((prev) => !prev)}
+        disabled={isDisabled}
       >
         Change to {isRed ? "Blue" : "Red"}
       </button>
+      <br />
+
+      <div>
+        <input
+          type="checkbox"
+          id="disable-button"
+          onChange={(e) => setIsDisabled(e.target.checked)}
+        />
+        <label htmlFor="disable-button">
+          {isDisabled ? "Enable" : "Disable"} button
+        </label>
+      </div>
     </div>
   );
 }
--- practice/button-color/src/App.test.jsx
+++ practice/button-color/src/App.test.jsx
@@ -10,3 +10,17 @@ test("Button click flow", () => {
   expect(buttonElement).toHaveTextContent(/red/i);
   expect(buttonElement).toHaveClass("blue");
 });
+
+test("Button disabled flow", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", { name: /disable/i });
+
+  expect(checkboxElement).not.toBeChecked();
+  expect(buttonElement).toBeEnabled();
+
+  fireEvent.click(checkboxElement);
+  expect(checkboxElement).toBeChecked();
+  expect(checkboxElement).toHaveAccessibleName(/enable/i); // For the label text
+  expect(buttonElement).toBeDisabled();
+});
